9edae250ca90c61c18e0e898f27c73587b884b4a
Kristina Diaz <kristina.diaz84@gmail.com>
2023-07-13 12:37:25 -0600

    finished building app

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let deckId;
+let remainingCards;
+let computerScore = 0;
+let myScore = 0;
 const newDeckBtn = document.getElementById('new-deck');
 const drawCardBtn = document.getElementById('draw-card');
 const cardImgContainer = document.getElementById('card-img');
 const gameResult = document.getElementById('game-result');
+const cardsRemaining = document.getElementById('remaining-cards');
+const computerScoreEl = document.getElementById('comp-score');
+const myScoreEl = document.getElementById('my-score');
 
 newDeckBtn.addEventListener('click', newDeckClick);
 drawCardBtn.addEventListener('click', drawTwoCards);
@@ -11,8 +17,8 @@ function newDeckClick() {
   fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
     .then(res => res.json())
     .then(data => {
+      cardsRemaining.textContent = `Remaining Cards: ${data.remaining}`;
       deckId = data.deck_id;
-      console.log(deckId);
     })
 };
 
@@ -21,6 +27,7 @@ function drawTwoCards() {
     .then(res => res.json())
     .then(data => {
       let cards = data.cards;
+      cardsRemaining.textContent = `Remaining Cards: ${data.remaining}`;
       cardImgContainer.children[0].innerHTML = `
         <img src=${cards[0].image} class="card" />
       `
@@ -29,6 +36,17 @@ function drawTwoCards() {
       `
       const winnerText = cardWinner(cards[0], cards[1]);
       gameResult.textContent = winnerText;
+
+      if(data.remaining === 0) {
+        drawCardBtn.disabled = true;
+        if(computerScore > myScore) {
+          gameResult.textContent = 'The computer won the game!';
+        } else if(myScore > computerScore) {
+          gameResult.textContent = 'You won the game!';
+        } else {
+          gameResult.textContent = "It's a tie game!";
+        }
+      }
     })
 };
 
@@ -39,9 +57,13 @@ function cardWinner(card1, card2) {
   const card2ValueIndex = valueOptions.indexOf(card2.value);
 
   if(card1ValueIndex > card2ValueIndex) {
-    return 'Card 1 wins!';
+    computerScore++;
+    computerScoreEl.textContent = `Computer Score: ${computerScore}`;
+    return 'Computer Wins!';
   } else if(card1ValueIndex < card2ValueIndex) {
-    return 'Card 2 wins!';
+    myScore++;
+    myScoreEl.textContent = ` My Score: ${myScore}`;
+    return 'You Win!';
   } else {
     return "War!";
   }
